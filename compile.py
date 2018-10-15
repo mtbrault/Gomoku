73f813f0f8aab61bac626292c6352b443da93e01
Matthieu <matthieu.brault@epitech.eu>
2018-10-15 17:09:20 +0200

    script python to compile

---
+++ compile.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os, sys
+
+def clean():
+	os.system("rm -f src/main.o src/board.o src/IA.o gomoku")
+
+def compile():
+	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/main.o src/main.cpp")
+	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/board.o src/board.cpp")
+	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/IA.o src/IA.cpp")
+	os.system("g++ src/main.o src/board.o src/IA.o -o gomoku -I./include -Wall -Wextra -Werror -std=c++14")
+
+def main():
+	if len(sys.argv) == 1:
+		compile()
+	elif sys.argv[1] == "fclean":
+		clean()
+	elif sys.argv[2] == "re":
+		compile()
+		clean()
+	
+
+if __name__ == '__main__':
+	main()
