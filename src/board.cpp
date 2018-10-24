b98808c0b2d2dd3b06965b60da50ed54ee97b08c
Matthieu <matthieu.brault@epitech.eu>
2018-10-24 21:50:25 +0200

    a

--- src/board.cpp
+++ src/board.cpp
@@ -62,16 +62,14 @@ float	Board::calcLine(int x, int y, State state, std::pair<int, int> dir)
 	std::array<float, 4>		listVal = {1.75, 1.5, 1.25, 1};
 	float						result = 0;
 	State						val;
-	State						lastVal;
 
 	for (int i = 1; i < 5; i++) {
 		if (x + (dir.first * i) < 0 || x + (dir.first * i) > _size || y + (dir.second * i) < 0 || y + (dir.second * i) > _size)
 			break ;
 		val = _board[x + (dir.first * i)][y + (dir.second * i)].getOwner();
-		lastVal = _board[x + (dir.first * (i - 1))][y + (dir.second * (i - 1))].getOwner();
 		if (val == state)
 			result += listVal[i - 1];
-		else if (val == State::EMPTY && (lastVal != State::EMPTY && lastVal != state))
+		else if (val == State::EMPTY)
 			result += 0.01;
 		else if (val != state)
 			break ;
@@ -80,10 +78,9 @@ float	Board::calcLine(int x, int y, State state, std::pair<int, int> dir)
 		if (x - (dir.first * i) < 0 || x - (dir.first * i) > _size || y - (dir.second * i) < 0 || y - (dir.second * i) > _size)
 			break ;
 		val = _board[x - (dir.first * i)][y - (dir.second * i)].getOwner();
-		lastVal = _board[x - (dir.first * (i - 1))][y - (dir.second * (i - 1))].getOwner();
 		if (val == state)
 			result += listVal[i - 1];
-		else if (val == State::EMPTY && (lastVal != State::EMPTY && lastVal != state))
+		else if (val == State::EMPTY)
 			result += 0.01;
 		else if (val != state)
 			break ;
