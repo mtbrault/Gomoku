044976b427bb68f7d252ef26a81854122bbf0f60
Matthieu <matthieu.brault@epitech.eu>
2018-11-05 14:11:24 +0100

    patch bttf

--- compile.py
+++ compile.py
@@ -2,15 +2,15 @@
 import os, sys
 
 def clean():
-	os.system("rm -f src/main.o src/Board.o src/IA.o src/Game.o src/Tile.o pbrain-PARIS-brault.matthieu.exe")
+	os.system("rm -f src/main.o src/board.o src/IA.o src/Game.o src/Tile.o pbrain-PARIS-brault.matthieu.exe")
 
 def compile():
 	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/main.o src/main.cpp")
-	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/Board.o src/Board.cpp")
+	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/board.o src/board.cpp")
 	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/IA.o src/IA.cpp")
 	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/Game.o src/Game.cpp")
 	os.system("g++  -I./include -Wall -Wextra -Werror -std=c++14  -c -o src/Tile.o src/Tile.cpp")
-	os.system("g++ src/main.o src/Game.o src/Board.o src/IA.o src/Tile.o -o pbrain-PARIS-brault.matthieu.exe -I./include -Wall -Wextra -Werror -std=c++14")
+	os.system("g++ src/main.o src/Game.o src/board.o src/IA.o src/Tile.o -o pbrain-PARIS-brault.matthieu.exe -I./include -Wall -Wextra -Werror -std=c++14")
 
 def main():
 	if len(sys.argv) == 1:
